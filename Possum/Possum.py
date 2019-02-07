c7b76a33e3c45299ad611b10e16d37a6c8e993cd
Steve Kramer <steve@newknowledge.io>
2019-02-07 10:57:56 -0600

    Updating Possum primitive to process the NASA_TestData.txt as a test.

--- Possum/Possum.py
+++ Possum/Possum.py
@@ -196,22 +196,21 @@ if __name__ == "__main__":
     # B - .csv file corpus example
     # 1 - FIRST CLEAN TWEETS AND SUBSAMPLE FOR LOCAL MEMORY LIMITATION REASONS
     # filename = "scripts/data/6_20_17_32_bp_content.txt"
-    filename = "scripts/data/pro_gun_text.csv"
+    filename = "data/NASA_TestData.txt"
     print("\n\n\nDEBUG::main::starting sumy .csv file summarization test...")
     TopicExtractor = Possum(method='lsa') # example non-default method specification
     df = pd.read_csv(filename, dtype=str, header=None)
-    MAX_N_ROWS = 10000
-    df_list = df.ix[np.random.choice(df.shape[0],MAX_N_ROWS,replace=False),1].tolist() # subsample tweets
+    #MAX_N_ROWS = 500
+    #df_list = df.ix[np.random.choice(df.shape[0],MAX_N_ROWS,replace=False),1].tolist() # subsample tweets
+    df_list = df.tolist()
     df_list = [TopicExtractor.clean_sentence(str(sentence)) + '.' for sentence in df_list] # clean tweets
-    # print("DEBUG::first 100 cleaned tweets:")
-    # print(df_list[:100])
     (pd.DataFrame(df_list)).to_csv("scripts/data/truncated_frame.csv",index=False) # write cleaned tweets to file
     # 2 - LOOK AT THE TOP 30 BIGRAMS
     ngram_counts = Counter(TopicExtractor.ngrams(text="".join(df_list).split(), n=2))
     print("The 30 most common word bigrams are:")
     print(ngram_counts.most_common(30))
     # 3 - PERFORM SUMMARIZATION/TOPIC EXTRACTION
-    sentences = TopicExtractor.ExtractivelySummarizeCorpus(corpus_path="scripts/data/truncated_frame.csv",HTML=False,sentence_count=30)
+    sentences = TopicExtractor.ExtractivelySummarizeCorpus(corpus_path="data/NASA_TestData.txt",HTML=False,sentence_count=30)
     print("These are the summary sentences:")
     print(sentences)
     extractedTopics = TopicExtractor.ExtractTopics(sentences)
