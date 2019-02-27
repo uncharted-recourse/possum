120bb62ac9e4b726bc8ca8d934956ca9e400302f
Steve Kramer <steve@newknowledge.io>
2019-02-27 11:19:53 -0600

    Changing version requirements for scipy and regex. Bumping base primitive version to 1.1.3.

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from distutils.core import setup
 
 setup(name='Possum',
-    version='1.1.2',
+    version='1.1.3',
     description='Possum - Post Summarization',
     packages=['Possum'],
     install_requires=['sumy >= 0.7.0',
-        'scipy >= 0.19.0',
+        'scipy==1.1.0',
         'numpy>=1.14.2',
         'scikit-learn>=0.18.1',
         'pandas>=0.19.2'
-        'nltk>=3.3'],
+        'nltk>=3.3',
+        'regex==2017.4.5'],
     include_package_data=True,
 )
